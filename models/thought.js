fb91215a842a916155f2a445fba4fbd2776b81e3
amymckellar <amy_mckellar@yahoo.com>
2023-04-15 17:09:18 -0500

    adding const ReactionSchema in the thought.js file in the models folder

---
+++ models/thought.js
@@ -0,0 +1,29 @@
+const { Schema, model, Types } = require("mongoose");
+
+const ReactionSchema = new Schema(
+  {
+    reactionID: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlenght: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
