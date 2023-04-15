26e44bc4b516ed6d2d1459f82758629ecae39ec3
amymckellar <amy_mckellar@yahoo.com>
2023-04-15 17:04:18 -0500

    added new users and delete users in the userController.js file

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,5 +28,61 @@ const userController = {
         });
     },
 
-    
+    // POST a new user to create a new one
+    createUser(req, res) {
+        user.create(req.body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    },
+
+    // PUT to update a user by id
+    updateUser(req, res) {
+        user.create(req.body)
+        .then(dbUserData) => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // DELETE to remove user by id
+    deleteUser(req, res) {
+        user.findOneAndDelete({_id: req.params.id})
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No user found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // POST to add a new friend
+    addFriend(req, res) {
+        user.findOneAndUpdate(
+            {_id: req.params.id},
+            {$push: {friends: req.params.friendId}},
+            {new: true}
+        )
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No user found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // DELETE to remove a friend
+    deleteFriend(req, res) {
+        user.findOneAndUpdate(
+            {_id: req.params.id},
+            {$pull: {friends: req.params.friendId}},
+            {new: true}
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = userController;
        
