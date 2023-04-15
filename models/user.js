8185ce78d897be4fa8b28af9f15d835ea79e2973
amymckellar <amy_mckellar@yahoo.com>
2023-04-15 17:15:34 -0500

    get total count of friend and adding const in the user.js file in the models folder

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+const thought = require("./Thought");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+\..+/],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of friends on retrieval
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// create the User model using the userSchema
+const User = model("User", userSchema);
+
+// export the User model
+
+module.exports = User;
